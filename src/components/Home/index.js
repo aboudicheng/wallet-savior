bcea28e04912e40876a866fd66eb7f02bdd25ea5
aboudicheng <aboudicheng@gmail.com>
2018-07-16 11:22:16 +0300

    change style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,21 @@ import FirstUse from './firstUseDialog'
 import withAuthorization from '../Session/withAuthorization';
 import firebase from 'firebase';
 import formatNumber from "format-number";
+import Button from '@material-ui/core/Button';
+import Edit from '@material-ui/icons/Edit'
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+    right: "1.8rem",
+    bottom: "1.8rem",
+    position: "absolute",
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
 
 const usersRef = firebase.database().ref('users/')
 
@@ -38,12 +53,17 @@ class HomePage extends Component {
 
   render() {
     const { totalAmount } = this.state
+    const { classes } = this.props
+
+    //TODO: insert, deposit, reset
     return (
       <div>
         <h1>My Wallet</h1>
         <span style={{ fontSize: "4rem" }}>{formatNumber({ prefix: "$" })(totalAmount.toFixed(2))}</span>
 
-        
+        <Button variant="fab" color="secondary" aria-label="Edit" className={classes.button}>
+          <Edit />
+        </Button>
 
         {this.state.firstUse &&
           <FirstUse open={this.state.open} handleClose={this.handleClose} setFirstUse={this.setFirstUse} />
@@ -62,5 +82,6 @@ const authCondition = (authUser) => !!authUser;
 
 export default compose(
   withAuthorization(authCondition),
-  connect(mapStateToProps)
+  connect(mapStateToProps),
+  withStyles(styles)
 )(HomePage);
