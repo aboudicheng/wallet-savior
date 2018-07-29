aab9b6eb1acfd0437c681a54b2651bd749bb9228
aboudicheng <aboudicheng@gmail.com>
2018-07-29 19:27:59 +0300

    able to change name

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import firebase from "firebase/app";
 import { connect } from "react-redux";
 import { compose } from "recompose";
-import Tooltip from "@material-ui/core/Tooltip";
-import Zoom from "@material-ui/core/Zoom";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Divider from "@material-ui/core/Divider";
+import TextField from "@material-ui/core/TextField";
+import Snackbar from "@material-ui/core/Snackbar";
 import DeleteAccount from "./deleteAccount";
 import PasswordChangeForm from "../PasswordChange";
 import withAuthorization from "../Session/withAuthorization";
+import MySnackbarContentWrapper from "../MySnackbarContentWrapper";
 
 const style = {
   width: 50,
@@ -15,34 +18,102 @@ const style = {
 };
 
 class AccountPage extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      isLoading: true,
+      snackbarOpen: false,
+    }
+  }
+  componentDidMount() {
+    firebase.database().ref(`users/${this.props.authUser.uid}/username`).once("value", snapshot => {
+      this.setState({ username: snapshot.val(), isLoading: false });
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({ username: e.target.value });
+  }
+
+  handleSnackbarClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    this.setState({ snackbarOpen: false });
+  };
+
+  onSubmit = (e) => {
+    const { username } = this.state;
+    if (username !== "") {
+      firebase.database().ref(`users/${this.props.authUser.uid}`).update({ username: username });
+      this.setState({ snackbarOpen: true });
+    }
+
+    e.preventDefault();
+  }
+
   render() {
+    const { username, isLoading, snackbarOpen } = this.state;
     const { authUser } = this.props;
-    console.log(authUser)
     return (
       <div>
-        {authUser.providerData[0].providerId !== "password" &&
-          <Tooltip TransitionComponent={Zoom} title={authUser.displayName}>
-            <img src={authUser.providerData[0].photoURL} alt={authUser.displayName} style={style} />
-          </Tooltip>
-          
-        }
-        <h3>{authUser.displayName ? authUser.displayName : authUser.email}</h3>
-
-        <span style={{ fontWeight: "bold" }}>Email: </span><p>{authUser.email}</p>
-        {authUser.providerData[0].providerId === "password" &&
+        {isLoading
+          ? <CircularProgress size={50} />
+          :
           <div>
-            <h2>Change Password</h2>
-            <PasswordChangeForm />
-            <Divider />
+            {authUser.providerData[0].providerId !== "password" &&
+              <img src={authUser.providerData[0].photoURL} alt={username} style={style} />
+            }
+
+            <form style={{ width: "100%" }} onSubmit={this.onSubmit}>
+              <TextField
+                margin="dense"
+                id="username"
+                label="Username"
+                type="text"
+                style={{ margin: "1.5rem" }}
+                value={username}
+                onChange={this.handleChange}
+              />
+            </form>
+
+            <span style={{ fontWeight: "bold" }}>Email: </span><span>{authUser.email}</span>
+            {authUser.providerData[0].providerId === "password" &&
+              <div>
+                <h2>Change Password</h2>
+                <PasswordChangeForm />
+                <Divider />
+              </div>
+            }
+
+            <div style={{
+              top: "0.8rem",
+              right: "0.5rem",
+              position: "absolute",
+            }}><DeleteAccount /></div>
+
+            {/*Snackbar poppup*/}
+            <Snackbar
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              open={snackbarOpen}
+              autoHideDuration={3000}
+              onClose={this.handleSnackbarClose}
+            >
+              <MySnackbarContentWrapper
+                onClose={this.handleSnackbarClose}
+                variant="success"
+                message="Operation successful!"
+              />
+            </Snackbar>
+
           </div>
         }
-
-        <div style={{
-          top: "0.8rem",
-          right: "0.5rem",
-          position: "absolute",
-        }}><DeleteAccount /></div>
-
       </div>
     )
   }
