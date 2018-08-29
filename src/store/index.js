99b618dc0a5aee62b66f44c79367278adfc7ea8c
aboudicheng <aboudicheng@gmail.com>
2018-08-29 18:21:56 +0300

    Check user locale

--- src/store/index.js
+++ src/store/index.js
@@ -6,17 +6,6 @@ import * as actions from "../constants/action_types";
 import { EN } from '../Languages/en';
 import { ZH_TW } from '../Languages/zh-TW';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
-
-const locale = navigator.language;
-
-const languages = {
-  "en": EN,
-  "zh-TW": ZH_TW
-};
-
-const localeData = languages[locale];
-
 function get_language(locale) {
   const dash_index = locale.indexOf('-')
   if (dash_index >= 0) {
@@ -24,6 +13,22 @@ function get_language(locale) {
   }
   return locale
 }
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+const languages = {
+  "en": EN,
+  "zh-TW": ZH_TW
+};
+
+let locale = navigator.language;
+//if the user's locale isn't one of the languages provided, set the default
+//language to English
+if (!languages.hasOwnProperty(locale)) {
+  locale = "en";
+}
+
+const localeData = languages[locale];
 
 store.dispatch({ type: actions.SET_LANGUAGE, language: get_language(locale), messages: localeData})
 
